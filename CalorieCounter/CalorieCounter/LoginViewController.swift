d5eb97877b75c381ade8ed02ef4b7e1ad6d34e42
kp1zano <kpizano@csumb.edu>
2016-11-22 14:07:46 -0800

    Added Login and Register Page

--- CalorieCounter/CalorieCounter/LoginViewController.swift
+++ CalorieCounter/CalorieCounter/LoginViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var userEmailTextField: UITextField!
+    @IBOutlet weak var userPasswordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +23,54 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func displayAlertMessage(userMessage:String){
+        
+        let myAlert = UIAlertController(title: "Alert", message: userMessage, preferredStyle: UIAlertControllerStyle.Alert);
+        
+        let okAction = UIAlertAction(title: "Alert", style: UIAlertActionStyle.Default, handler: nil);
+        myAlert.addAction(okAction);
+        self.presentViewController(myAlert, animated: true, completion: nil);
+    }
+    
+    @IBAction func loginButtonTapped(sender: AnyObject) {
+        
+        /*
+         * Grab the information that the user inserted.
+         */
+        let userEmail = userEmailTextField.text;
+        let userPassword = userPasswordTextField.text;
+        
+        /*
+         * Make sure none of the fields are empty.
+         */
+        if(userEmail!.isEmpty || userPassword!.isEmpty){
+            displayAlertMessage("All fields are required");
+        }
+        
+        /*
+         * Read from hte local DB for the inputs.
+         */
+        let userEmailStored = NSUserDefaults.standardUserDefaults().stringForKey("userEmail");
+        let userPasswordStored = NSUserDefaults.standardUserDefaults().stringForKey("userPassword");
+        
+        /*
+         * Make sure stored credentials match input.
+         */
+        if(userEmailStored == userEmail){
+            
+            if(userPasswordStored == userPassword){
+                // Login Successfull.
+                NSUserDefaults.standardUserDefaults().setBool(true, forKey: "isUserLoggedIn");
+                NSUserDefaults.standardUserDefaults().synchronize();
+                self.dismissViewControllerAnimated(true, completion: nil);
+                return;
+            }
+            displayAlertMessage("Incorrect Password or Email");
+        }
+        displayAlertMessage("No email found");
+        
+        
+    }
 
     /*
     // MARK: - Navigation
--- CalorieCounter/CalorieCounter/RegisterPageViewController.swift
+++ CalorieCounter/CalorieCounter/RegisterPageViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class RegisterPageViewController: UIViewController {
 
+    @IBOutlet weak var userEmailTextField: UITextField!
+    @IBOutlet weak var userPasswordTexField: UITextField!
+    @IBOutlet weak var userRepeatPasswordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +25,70 @@ class RegisterPageViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func registerButtonTapped(sender: AnyObject) {
+        
+        /*
+         * Get users data and store it.
+         */
+        let userEmail = userEmailTextField.text;
+        let userPassword = userPasswordTexField.text;
+        let userRepeatedPassword = userRepeatPasswordTextField.text;
+        
+        /*
+         * Check for empty fields.
+         */
+        if(userEmail!.isEmpty || userPassword!.isEmpty || userRepeatedPassword!.isEmpty){
+            /*
+             * Send an alert message.
+             */
+            displayAlertMessage("All fields are required");
+            return;
+        }
+        
+        /*
+         * Check if the passwords match.
+         */
+        print(userRepeatedPassword);
+        print(userPassword);
+        print(userEmail);
+        if(userPassword! != userRepeatedPassword!){
+            /*
+             * Send alert message;
+             */
+            displayAlertMessage("Passwords do not match");
+            return;
+        }
+        
+        /*
+         * Store data locally.
+         */
+        
+        NSUserDefaults.standardUserDefaults().setObject(userEmail, forKey: "userEmail");
+        NSUserDefaults.standardUserDefaults().setObject(userPassword, forKey: "userPassword");
+        NSUserDefaults.standardUserDefaults().synchronize();
+        
+        /*
+         * Display confirmation alert.
+         */
+        let myAlert = UIAlertController(title: "Alert", message: "Profile Successfully Created", preferredStyle: UIAlertControllerStyle.Alert);
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default){
+            action in self.dismissViewControllerAnimated(true, completion: nil);
+            }
+        myAlert.addAction(okAction);
+        self.presentViewController(myAlert, animated: true, completion: nil);
+        
+    }
+    
+    
+    func displayAlertMessage(userMessage:String){
+        
+        let myAlert = UIAlertController(title: "Alert", message: userMessage, preferredStyle: UIAlertControllerStyle.Alert);
+        
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil);
+        
+        myAlert.addAction(okAction);
+        self.presentViewController(myAlert, animated: true, completion: nil);
+    }
 
     /*
     // MARK: - Navigation
