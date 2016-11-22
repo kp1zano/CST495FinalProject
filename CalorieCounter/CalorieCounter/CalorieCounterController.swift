5b5796dcb036a51ea0c069c87786843e250b3e2b
Yarely Chino <yarely2011@yahoo.com>
2016-11-22 15:18:59 -0800

    Added new board for calorie daily count

---
+++ CalorieCounter/CalorieCounter/CalorieCounterController.swift
@@ -0,0 +1,19 @@
+
+//
+//  CalorieCounterController.swift
+//  CalorieCounter
+//
+//  Created by Yarely on 11/22/16.
+//  Copyright © 2016 CSUMB. All rights reserved.
+//
+
+import Foundation
+
+class CalorieCounter{
+    
+    func initCalorieCounter(){
+        
+    }
+    
+}
+
---
+++ CalorieCounter/TableViewController.swift
@@ -0,0 +1,79 @@
+//
+//  TableViewController.swift
+//  CalorieCounter
+//
+//  Created by Yarely on 11/22/16.
+//  Copyright © 2016 CSUMB. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    var datePickerHidden = true;
+    @IBOutlet weak var totalCalories: UILabel!
+    @IBOutlet weak var breakCalories: UILabel!
+    @IBOutlet weak var lunchCalories: UILabel!
+    @IBOutlet weak var dinnerCalories: UILabel!
+    @IBOutlet weak var snacksCalories: UILabel!
+    
+
+    @IBAction func datePickerValue(sender: UIDatePicker) {
+        datePickerChanged()
+    }
+    /*
+     Changes datepicker label
+     */
+    func datePickerChanged () {
+        
+       let dateFormatter = NSDateFormatter()
+       dateFormatter.dateFormat = "MM/dd/yyyy"
+       detailLabel.text = dateFormatter.stringFromDate(datePicker.date)
+       //Debugin:
+        debugPrint(dateFormatter.stringFromDate(datePicker.date))
+        
+    }
+    
+    /*
+     View to check if date did load. (Initializer)
+     */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        datePickerChanged()
+    }
+    
+    /*
+     Select the row at index path, this will trigger the hiding method
+     */
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            toggleDatepicker()
+        }
+    }
+    /*
+     
+     */
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if datePickerHidden && indexPath.section == 0 && indexPath.row == 1 {
+            return 0
+        }
+        else {
+            return super.tableView(tableView, heightForRowAtIndexPath: indexPath)
+        }
+    }
+    
+    /*
+     
+     */
+    func toggleDatepicker() {
+        
+        datePickerHidden = !datePickerHidden
+        tableView.beginUpdates()
+        tableView.endUpdates()
+        
+    }
+    
+
+}
