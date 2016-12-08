bae9c2b6e1693e94ef414232e5ce9a98c7fdaeed
pierater <martin.almaraz@gmail.com>
2016-12-07 23:14:28 -0800

    can now count the total calories in the views

--- CalorieCounter/TableViewController.swift
+++ CalorieCounter/TableViewController.swift
@@ -46,7 +46,7 @@ class TableViewController: UITableViewController {
         super.viewDidLoad()
         datePickerChanged()
         self.title = "Home Page"
-       
+        loadCounts()
     }
     
     /*
@@ -118,6 +118,90 @@ class TableViewController: UITableViewController {
         if(!isUserLoggedIn){
             self.performSegueWithIdentifier("loginView", sender: self);
         }
+        
+        loadCounts()
+    }
+    
+    /*
+     * Load the current cal count of the tables
+     * and set them
+     * I think this is a bit messy with all the if let..if let... etc + the loops
+     * so if anyone knows a cleaner way of doing this, then...		
+     */
+    
+    func loadCounts() {
+        print("LOADING COUNTS")
+        
+        let username = NSUserDefaults.standardUserDefaults().stringForKey("userEmail")
+        var breakfastFoods = [Food]()
+        var lunchFoods = [Food]()
+        var dinnerFoods = [Food]()
+        var snackFoods = [Food]()
+        
+        if let loadedDataFoods = NSUserDefaults.standardUserDefaults().dataForKey("foods\(username)Optional(\"BreakFast Calories\")") {
+            
+            if let loadedFoods = NSKeyedUnarchiver.unarchiveObjectWithData(loadedDataFoods) as? [Food] {
+                print("Loaded data from userDefaults BREAKFAST \(loadedFoods)")
+                breakfastFoods = loadedFoods
+            }
+        }
+        
+        if let loadedDataFoods = NSUserDefaults.standardUserDefaults().dataForKey("foods\(username)Optional(\"Lunch Calories\")") {
+            
+            if let loadedFoods = NSKeyedUnarchiver.unarchiveObjectWithData(loadedDataFoods) as? [Food] {
+                print("Loaded data from userDefaults LUNCH \(loadedFoods)")
+                lunchFoods = loadedFoods
+            }
+        }
+        
+        if let loadedDataFoods = NSUserDefaults.standardUserDefaults().dataForKey("foods\(username)Optional(\"Dinner Calories\")") {
+            
+            if let loadedFoods = NSKeyedUnarchiver.unarchiveObjectWithData(loadedDataFoods) as? [Food] {
+                print("Loaded data from userDefaults DINNER \(loadedFoods)")
+                dinnerFoods = loadedFoods
+            }
+        }
+        
+        if let loadedDataFoods = NSUserDefaults.standardUserDefaults().dataForKey("foods\(username)Optional(\"Snacks Calories\")") {
+            
+            if let loadedFoods = NSKeyedUnarchiver.unarchiveObjectWithData(loadedDataFoods) as? [Food] {
+                print("Loaded data from userDefaults SNACKS \(loadedFoods)")
+                snackFoods = loadedFoods
+            }
+        }
+        
+        var breakfastTot = 0
+        var lunchTot = 0
+        var dinnerTot = 0
+        var snackTot = 0
+        
+        for item in breakfastFoods {
+            print("\(Int(item.calories)!)")
+            breakfastTot += Int(item.calories)!
+        }
+        
+        for item in lunchFoods {
+            print("\(Int(item.calories)!)")
+            lunchTot += Int(item.calories)!
+        }
+        
+        for item in dinnerFoods {
+            print("\(Int(item.calories)!)")
+            dinnerTot += Int(item.calories)!
+        }
+        
+        for item in snackFoods {
+            print("\(Int(item.calories)!)")
+            snackTot += Int(item.calories)!
+        }
+        
+        self.breakCalories.text = "\(breakfastTot)"
+        self.lunchCalories.text = "\(lunchTot)"
+        self.dinnerCalories.text = "\(dinnerTot)"
+        self.snacksCalories.text = "\(snackTot)"
+        self.totalCalories.text = "\(breakfastTot + lunchTot + dinnerTot + snackTot)"
+        
+        print("DONE LOADING COUNTS TOT: \(breakfastTot + lunchTot + dinnerTot + snackTot)")
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
